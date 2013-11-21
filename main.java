8536f0c59c9dee3c9f07ea3c4398a15f4cb4425e
Jacks624 <jas78@aber.ac.uk>
2013-11-21 19:23:38 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.awt.Container;
+import java.util.*;
+
+import javax.swing.*;
+
+public class main {
+
+	public static void main(String[] args) {
+Project pj = new Project();
+	}
+
+
+
+	
+}
